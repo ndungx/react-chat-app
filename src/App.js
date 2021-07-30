3694f134f755a2abde7af4498361b379a55d4252
ndungx <dpron12345@gmail.com>
2021-07-30 16:01:59 +0700

    add AuthProvider and component ChatRoom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import AuthProvider from './Context/AuthProvider';
+import ChatRoom from './Components/ChatRoom';
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <Route component={Login} path="/login" />
+          <Route component={ChatRoom} path="/" />
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
