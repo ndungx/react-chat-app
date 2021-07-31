7c96a65a2a74e562a029fa9d0f19711012495e65
ndungx <dpron12345@gmail.com>
2021-07-31 10:37:44 +0700

    Change to firebase emulator

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,5 +19,10 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
+auth.useEmulator('http://localhost:9099');
+if (window.location.hostname === 'localhost') {
+    db.useEmulator('localhost', '8080');
+}
+
 export { db, auth }
 export default firebase;
