6452986d42eb94c4341cd9a572430c9f639ac5c1
ndungx <dpron12345@gmail.com>
2021-07-31 10:14:59 +0700

    Finish component RoomList of component Sidebar

---
+++ src/Components/ChatRoom/RoomList.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Collapse, Typography, Button } from 'antd'
+import styled from 'styled-components';
+import { PlusSquareOutlined } from '@ant-design/icons'
+import { AuthContext } from '../../Context/AuthProvider';
+import useFirestore from '../../hooks/useFirestore';
+import { AppContext } from '../../Context/AppProvider';
+
+const { Panel } = Collapse;
+
+const PanelStyled = styled(Panel)`
+    %%% {
+        .ant-collapse-content-box {
+            padding: 0 40px;
+        }
+
+        .add-room {
+            color: #fff;
+            padding: 0;
+        }
+    }
+`;
+
+const LinkStyled = styled(Typography.Link)`
+    display: block;
+    margin-bottom: 5px;
+    color: #313131;
+`
+
+const RoomListHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export default function RoomList() {
+    const { rooms, setIsAddRoomVisible, setSelectedRoomId } = React.useContext(AppContext);
+    console.log(rooms);
+
+    const handleAddRoom = () => {
+        setIsAddRoomVisible(true);
+    }
+
+    return (
+        <Collapse ghost defaultActiveKey={['1']}>
+            <PanelStyled header="Room List" key="1">
+                <RoomListHeader>
+                    <Typography.Text>Chats</Typography.Text>
+                    <Button
+                        type="text"
+                        icon={<PlusSquareOutlined />}
+                        className="add-room"
+                        onClick={handleAddRoom}
+                    >
+                    </Button>
+                </RoomListHeader>
+
+                {
+                    rooms.map(room => (
+                        <LinkStyled key={room.id} onClick={() => setSelectedRoomId(room.id)}>
+                            {room.name}
+                        </LinkStyled>)
+                    )
+                }
+
+            </PanelStyled>
+        </Collapse>
+    )
+}
