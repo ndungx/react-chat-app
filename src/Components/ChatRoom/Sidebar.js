bbcdcd562ee5de4af2b682c665d946b90e94c71a
ndungx <dpron12345@gmail.com>
2021-07-31 10:26:55 +0700

    Finished Sidebar component

--- src/Components/ChatRoom/Sidebar.js
+++ src/Components/ChatRoom/Sidebar.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Row, Col } from 'antd'
+import UserInfo from './UserInfo'
+import RoomList from './RoomList'
+import styled from 'styled-components'
 
+
+const SideBarStyled = styled.div`
+    background: #b2b2b2;
+    color: #fff;
+    height: 100vh;
+`;
 export default function Sidebar() {
     return (
-        <div>
-            This is Sidebar
-        </div>
+        <SideBarStyled>
+            <Row>
+                <Col span={24}>
+                    <UserInfo />
+                </Col>
+                <Col span={24}>
+                    <RoomList />
+                </Col>
+            </Row>
+        </SideBarStyled>
     )
 }
