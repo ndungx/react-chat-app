bc4049cc8ed7f1e57b45e7c961e3c507c2cce0b3
ndungx <dpron12345@gmail.com>
2021-07-31 11:07:28 +0700

    add component AppProvider surround component of chat app to get data

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Login from './Components/Login';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AuthProvider from './Context/AuthProvider';
 import ChatRoom from './Components/ChatRoom';
+import AppProvider from './Context/AppProvider';
+import AddRoomModal from './Components/Modals/AddRoomModal';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Switch>
-          <Route component={Login} path="/login" />
-          <Route component={ChatRoom} path="/" />
-        </Switch>
+        <AppProvider>
+          <Switch>
+            <Route component={Login} path="/login" />
+            <Route component={ChatRoom} path="/" />
+          </Switch>
+          <AddRoomModal />
+        </AppProvider>
       </AuthProvider>
     </BrowserRouter>
   );
