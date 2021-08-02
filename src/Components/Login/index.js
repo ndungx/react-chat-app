8828a5497745660fba593e9336a75c0a1d25be5e
ndungx <dpron12345@gmail.com>
2021-08-02 11:51:27 +0700

    Fix text

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -29,7 +29,7 @@ export default function Login() {
             <Row justify='center' style={{ height: 800 }}>
                 <Col span={8}>
                     <Title style={{ textAlign: 'center' }} level={3}>
-                        Fun Chat
+                        React Chat App
                     </Title>
                     <Button style={{ width: '100%', marginBottom: 5 }}
                         onClick={() => handleLogin(googleProvider)}
